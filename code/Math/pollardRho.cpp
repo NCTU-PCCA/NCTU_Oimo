d223ce5cc7921ce4a5a2dbf0848fc31223006a70
fsh0524 <fsh0524@gmail.com>
2018-12-11 02:57:30 +0800

    2018 ICPC Asia Singapore Regional

--- code/Math/pollardRho.cpp
+++ code/Math/pollardRho.cpp
@@ -1,19 +1,18 @@
-// from PEC
 // does not work when n is prime
-Int f(Int x, Int mod){
-	return add(mul(x, x, mod), 1, mod);
+inline LL f(LL x , LL mod) {
+return (x * x % mod + 1) % mod;
 }
-Int pollard_rho(Int n) {
-	if ( !(n & 1) ) return 2;
-  while (true) {
-    Int y = 2, x = rand()%(n-1) + 1, res = 1;
-    for ( int sz = 2 ; res == 1 ; sz *= 2 ) {
-      for ( int i = 0 ; i < sz && res <= 1 ; i++) {
-        x = f(x, n);
-        res = __gcd(abs(x-y), n);
+inline LL pollard_rho(LL n) {
+  if(!(n&1)) return 2;
+  while(true) {
+    LL y = 2 , x = rand() % (n - 1) + 1 , res = 1;
+    for(int sz = 2; res == 1; sz *= 2) {
+      for(int i = 0; i < sz && res <= 1; i++) {
+        x = f(x , n);
+        res = __gcd(abs(x - y) , n);
       }
       y = x;
     }
-    if ( res != 0 && res != n ) return res;
+    if (res != 0 && res != n) return res;
   }
 }
