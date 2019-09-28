a031619d150428fec6a858e3c87bc0abbb0942f2
stevenyu253436 <stevenyu.supreme@gmail.com>
2019-09-28 21:44:10 +0800

    v4

--- code/Flow/Min Cost Flow.cpp
+++ code/Flow/Min Cost Flow.cpp
@@ -1,83 +1,61 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define int long long
-typedef pair<int,int> P;
-struct edge{
-    edge(){}
-    edge(int a,int b,int c,int d):to(a),cap(b),cost(c),rev(d){}
-    int to,cap,cost,rev;
-};
-#define V 210
-#define inf 1000000000000000
-vector<edge> g[V];
-int h[V],dist[V],prev_v[V],prev_e[V];
-void add_edge(int from,int to,int cap,int cost){
-    g[from].push_back(edge(to,cap,cost,g[to].size()));
-    g[to].push_back(edge(from,0,-cost,g[from].size()-1));
-}
-int min_costflow(int s,int t,int f){
-    int res=0;
-    memset(h,0,sizeof(h));
-    while(f>0){
-        priority_queue<P,vector<P>,greater<P> >que;
-        fill(dist,dist+V,inf);
-        dist[s]=0;
-        que.push(P(dist[s],s));
-        while(!que.empty()){
-            P p=que.top();
-            que.pop();
-            int v=p.second;
-            if(dist[v]<p.first)continue;
-            for(int i=0;i<g[v].size();++i){
-                edge &e=g[v][i];
-                if(e.cap>0&&dist[e.to]>dist[v]+e.cost+h[v]-h[e.to]){
-                    dist[e.to]=dist[v]+e.cost+h[v]-h[e.to];
-                    prev_v[e.to]=v;
-                    prev_e[e.to]=i;
-                    que.push(P(dist[e.to],e.to));
+// long long version
+typedef pair<long long, long long> pll;
+struct CostFlow {
+    static const int MAXN = 350;
+    static const long long INF = 1LL<<60;
+    struct Edge {
+        int to, r;
+        long long rest, c;
+    };
+    int n, pre[MAXN], preL[MAXN]; bool inq[MAXN];
+    long long dis[MAXN], fl, cost;
+    vector<Edge> G[MAXN];
+    void init() {
+        for ( int i = 0 ; i < MAXN ; i++) G[i].clear();
+    }
+    void add_edge(int u, int v, long long rest, long long c) {
+        G[u].push_back({v, (int)G[v].size(), rest, c});
+        G[v].push_back({u, (int)G[u].size()-1, 0, -c});
+    }
+    pll flow(int s, int t) {
+        fl = cost = 0;
+        while (true) {
+            fill(dis, dis+MAXN, INF);
+            fill(inq, inq+MAXN, 0);
+            dis[s] = 0;
+            queue<int> que;
+            que.push(s);
+            while ( !que.empty() ) {
+                int u = que.front(); que.pop();
+                inq[u] = 0;
+                for ( int i = 0 ; i < (int)G[u].size() ; i++) {
+                    int v = G[u][i].to;
+                    long long w = G[u][i].c;
+                    if ( G[u][i].rest > 0 && dis[v] > dis[u] + w) {
+                        pre[v] = u; preL[v] = i;
+                        dis[v] = dis[u] + w;
+                        if (!inq[v]) {
+                            inq[v] = 1;
+                            que.push(v);
+                        }
+                    }
                 }
             }
-        }
-        if(dist[t]==inf) return -1;
-        for(int v=0;v<V;++v)h[v]+=dist[v];
-        int d=f;
-        for(int v=t;v!=s;v=prev_v[v]) d=min(d,g[prev_v[v]][prev_e[v]].cap);
-        f-=d;
-        res+=d*h[t];
-        for(int v=t;v!=s;v=prev_v[v]){
-            edge &e=g[prev_v[v]][prev_e[v]];
-            e.cap-=d;
-            g[v][e.rev].cap+=d;
-        }
-    }
-    return res;
-}
-#undef int
-int main()
-{
-#define int long long
-    int T,n,m,cost,l,s,t,ans;
-    cin>>T;
-    while(T--){
-    	cin>>n>>m;
-        for(int q=0;q<V;++q)g[q].clear();
-        s=m+n;
-        t=m+n+1;
-        for(int i=0;i<n;++i)
-        	for(int j=0;j<m;++j){
-        		cin>>cost;
-        		if(cost>0)
-        			add_edge(n+j,i,1,cost);
-        	}
-        for(int i=0;i<m;++i){
-        	cin>>l;
-        	add_edge(s,n+i,l,0);
-        }
-        for(int i=0;i<n;++i)
-        	add_edge(i,t,1,0);
-        ans=min_costflow(s,t,n);
-        cout<<ans<<endl;
-    }
-    return 0;
-}
 
+            if (dis[t] == INF) break;
+            long long tf = INF;
+            for (int v = t, u, l ; v != s ; v = u ) {
+                u = pre[v]; l = preL[v];
+                tf = min(tf, G[u][l].rest);
+            }
+            for (int v = t, u, l ; v != s ; v = u ) {
+                u = pre[v]; l = preL[v];
+                G[u][l].rest -= tf;
+                G[v][G[u][l].r].rest += tf;
+            }
+            cost += tf * dis[t];
+            fl += tf;
+        }
+        return {fl, cost};
+    }
+} flow;
